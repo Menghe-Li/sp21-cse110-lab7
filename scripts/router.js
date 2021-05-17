af7ffc4e05c27cc571da0e7b11a632fdf527f758
Menghe-Li <82562992+Menghe-Li@users.noreply.github.com>
2021-05-16 20:52:30 -0700

    fix minor issue

--- scripts/router.js
+++ scripts/router.js
@@ -7,27 +7,27 @@ export const router = {};
  */
 router.setState = function(state) {
   const page_title = document.querySelector("h1");
-  //  default
-  if (state == null) {
+  //  main page
+  if (state.page_title == "Journal_Entries") {
     history.pushState(null, "", "/");
-    page_title.textContent = "Journal Entries";
-    document.body.className = "";
+    page_title.textContent = state.page_title;
+    document.body.className = state.class_name;
   }
   // Setting page
   else if (state.page_title == "Settings") {
     history.pushState(state, "Settings", "#settings");
     page_title.textContent = state.page_title;
-    document.body.className = state.className;
+    document.body.className = state.class_name;
   }
   // individual entry page
   else {
+    const entry_page = document.querySelector("entry-page");
+    let singal_entry_page = document.createElement("entry-page");
     history.pushState(state, state.page_title, state.entry_number);
     page_title.textContent = state.page_title;
-    document.body.className = state.className;
-    let entry_page = document.querySelector("entry-page");
+    document.body.className = state.class_name;
     entry_page.remove();
-    entry_page = document.createElement("entry-page");
-    document.body.appendChild(entry_page);
-    entry_page.entry = state.entry_data;
+    document.body.appendChild(singal_entry_page);
+    singal_entry_page.entry = state.entry_data;
   }  
 }
